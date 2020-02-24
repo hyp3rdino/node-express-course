5ce49fa99692972417c2701792a36526408529de
Markus Kortlang <markus.kortalng@getslash.de>
2020-02-24 20:31:08 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 
 
@@ -18,6 +19,30 @@ app.get('/users/:id', (req,res) => {
         user: req.params.id
     })
 });
+
+app.post( '/login', (req, res) => {
+    const username = req.body.username
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username === mockUsername && password === mockPassword){
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+
+})
+
+
 
 app.listen(5101, () => console.log('server up!'));
 
